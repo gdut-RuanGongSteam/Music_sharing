7a9a593793bfd2c4ff172038eebd15443e227da7
Sabot1203 <sabot_v@163.com>
2020-05-06 08:36:29 +0800

    导入依赖包

--- src/test/java/com/zhuanye/music_system/MusicSystemApplicationTests.java
+++ src/test/java/com/zhuanye/music_system/MusicSystemApplicationTests.java
@@ -1,13 +1,1 @@
-package com.zhuanye.music_system;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class MusicSystemApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
